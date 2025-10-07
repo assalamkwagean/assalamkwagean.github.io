f5e8f801c1332401b8ba4f963d17f7652cb0bf2c
assalamkwagean <kwageanassalam@gmail.com>
2025-10-07 08:24:37 +0700

    Delete pembayaran-pondok directory

--- pembayaran-pondok/assets/js/config.js
+++
@@ -1,5 +0,0 @@
-// Konfigurasi API
-const API_CONFIG = {
-    URL: 'https://script.google.com/macros/s/AKfycbyU-zbb3QE0WxhOwQoX-PCbg655VBLJkOQxOqgUsYE5IgIh6ilF-cOVS38US7r07atg/exec', // Ganti dengan URL Web App Anda
-    KEY: 'G4F8E9D2C1A3B4C5D9E8F7A6B5C4D3E2F1G2H3I4J5K6L7M8N9O0P' // Ganti dengan API Key yang sama dengan di backend
-};
