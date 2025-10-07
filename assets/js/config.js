ddb4a6cf75f76b2e0ce0e7d01126b630bd813ee9
assalamkwagean <kwageanassalam@gmail.com>
2025-10-07 12:25:09 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,7 @@
 // Konfigurasi API
 const API_CONFIG = {
-    URL: 'https://script.google.com/macros/s/AKfycby1m7n1NzPaMmab-ZoDwUhwE-D82WbrelhdBvMEaMlSyDrHGOrnnLnQSj4JHnXCzTlr/exec', // Ganti dengan URL Web App Anda
+    URL: 'https://script.google.com/macros/s/AKfycbz8jO_1QPuxjNLPGQE4X-FlBuFl6clwDa599fCuBYSo5a5cW1IbOCIoGzpH0XzIsmhd/exec', // Ganti dengan URL Web App Anda
     KEY: 'PondokAsSalam2025!SecretKey@DigitalPayment' // Ganti dengan API Key yang sama dengan di backend
 
 };
+
