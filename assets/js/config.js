b0f2465bf7dab82f134e291fb66a848769c29fa6
assalamkwagean <kwageanassalam@gmail.com>
2025-10-09 17:37:41 +0700

    Delete assets directory

--- assets/js/config.js
+++
@@ -1,3 +0,0 @@
-// Configuration for API URL and API Key - set these after deploying your Apps Script Web App
-const API_URL = "https://script.google.com/macros/s/AKfycbyRBSSkIuJ3AbOJDIRN5tACJQR3J_dHGcvqnUaCpstJOR-E3YK9TgWSs6txBOdhLh25/exec"; // e.g. https://script.google.com/macros/s/AKfy.../exec
-const API_KEY = "PondokAsSalam2025!SecretKey@DigitalPayment"; // must match API_KEY in kode.gs
