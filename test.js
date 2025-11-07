67b1a79714cdc20aed7662f9c0049db9f814ca61
assalamkwagean <kwageanassalam@gmail.com>
2025-11-07 17:42:06 +0700

    Delete test.js

--- test.js
+++
@@ -1,6 +0,0 @@
-describe("Configuration", function() {
-  it("should have getUserData and getUserType functions defined", function() {
-    expect(typeof getUserData).toBe('function');
-    expect(typeof getUserType).toBe('function');
-  });
-});
