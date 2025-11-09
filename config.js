72dbe28af25b855d3bdceca7d4a5df732924d399
assalamkwagean <kwageanassalam@gmail.com>
2025-11-09 15:19:27 +0700

    Update backend URL in config.js

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 // Ganti URL berikut dengan URL Web App Google Apps Script yang Anda deploy
-window.__BACKEND_URL__ = 'https://script.google.com/macros/s/AKfycbyeoZlSGfQCCtyhL2pVFqhBpoFUUSSyfC8vOSfZCkHzj-RSXPp3m-6fzhp9ayrNlNGjwQ/exec';
+window.__BACKEND_URL__ = 'https://script.google.com/macros/s/AKfycbxOb17F9d9Wm3MezNYdBIKnc-_H7PXkn4qfaD8Onx4eE5Idk4ATQek5_V_9jNAgjrQONw/exec';
 
 // Konfigurasi client-side tambahan (bisa diubah)
 window.APP_CONFIG = {
   LOGO_FILE_NAME: 'logo_pondok.webp'
 };
+
 
