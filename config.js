9005088f3785c0b0ae6b35913998e4192bc54f6b
assalamkwagean <assalamkwagean@gmail.com>
2025-11-09 14:42:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Ganti URL berikut dengan URL Web App Google Apps Script yang Anda deploy
-window.__BACKEND_URL__ = 'https://script.google.com/macros/s/AKfycbwFUyY_2TIz5jQ4_DmJtBVNreFIvcUWLaEoFteT_1Mm7V3EqnoqPaiKwHE0Yxq1IpAweQ/exec';
+window.__BACKEND_URL__ = 'https://script.google.com/macros/s/AKfycbzjEKjgB0BWKlQcClhU6gslu2JLAXCe_goFMeev4TxHYKXH5o9AUwSq5dl4sgQsrwhu3w/exec';
 
 // Konfigurasi client-side tambahan (bisa diubah)
 window.APP_CONFIG = {
